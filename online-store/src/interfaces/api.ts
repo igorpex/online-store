aaedddf3c91960406cefc55652d8c4a77f6cadd3
igorpex <igor.bogdanov@gmail.com>
2022-07-09 11:12:56 +0300

    feat: add api interfaces

--- online-store/src/interfaces/api.ts
+++ online-store/src/interfaces/api.ts
@@ -1,7 +1,9 @@
-export interface Products {
+export interface Product {
     "id": string;
     "name": string;
     "price": number;
+    // [key: number]: string;
+    // [price: number]: string;
     "image": string;
     "colors": string[];
     "company": string;
