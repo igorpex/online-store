633388bbdca97e68253074d10fbfa0dc605d3fa7
igorpex <igor.bogdanov@gmail.com>
2022-07-09 11:13:49 +0300

    refactor: add some internal testing data

---
+++ online-store/src/data/somedata.ts
@@ -0,0 +1,64 @@
+import { handlePriceSliderChange, handleSearchFilter, handleCategoryFilter, handleCompanyFilter, handleColorFilter } from '../handlers/handlers';
+let filters = [
+
+    {
+        name: 'search',
+        type: 'text',
+        containerSelectors: ['.filters__search'],
+        elementClasses: ['.filters__search-input'],
+        handler: handleSearchFilter,
+    },
+    {
+        name: 'category',
+        type: 'checkbox',
+        containerSelectors: ['.filters__category'],
+        elementClasses: ['.filters__category-input'],
+        handler: handleCategoryFilter,
+    },
+    {
+        name: 'company',
+        type: 'checkbox',
+        containerSelectors: ['.filters__company'],
+        elementClasses: ['.filters__company-input'],
+        // handler: handleCompanyFilter,
+    },
+    {
+        name: 'color',
+        type: 'button',
+        containerSelectors: ['.filters__color'],
+        elementClasses: ['.filters__colors-button'],
+        handler: handleCategoryFilter,
+    },
+
+]
+
+let filtersObj = {
+    'search': {
+        type: 'search',
+        containerSelectors: ['.filters__search'],
+        elementClasses: ['.filters__search-input'],
+        handler: handleSearchFilter,
+    },
+    'category': {
+        type: 'checkbox',
+        containerSelectors: ['.filters__category'],
+        elementClasses: ['.filters__category-input'],
+        handler: handleCategoryFilter,
+    },
+    'color': {
+        type: 'button',
+        containerSelectors: ['.filters__color'],
+        elementClasses: ['.filters__colors-button'],
+        handler: handleCategoryFilter,
+    },
+}
+
+const rangeFilters = [
+    {
+        name: 'price',
+        type: 'range',
+        containerSelectors: ['.filters__price-range'],
+        elementClasses: ['.filters__price-range-input'],
+        handler: handlePriceSliderChange,
+    }
+]
