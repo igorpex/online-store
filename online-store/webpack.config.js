ef20d0558ee020bd3c73c83b99133b3d6e5002a1
igorpex <igor.bogdanov@gmail.com>
2022-07-14 19:35:04 +0300

    refactor: play with sass material design elements

--- online-store/webpack.config.js
+++ online-store/webpack.config.js
@@ -16,7 +16,21 @@ const baseConfig = {
             },
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
             },
         ],
     },
